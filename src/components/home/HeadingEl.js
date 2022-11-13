f7c0115654f4c5d274ff45a8f8136e22f90d0b2f
nishkarta <nishkarta@gmail.com>
2022-11-14 01:40:00 +0700

    test

--- src/components/home/HeadingEl.js
+++ src/components/home/HeadingEl.js
@@ -1,7 +1,7 @@
 export default function HeadingEl() {
     return (
         <div className="d-flex align-items-center h-60 pt-5">
-            <p className="ff-vlk fs-43 w-60 mx-auto  h-100">With us, you can shop online & help save your high street at the same time</p>
+            <p className="ff-vlk fs-43 w-60 mx-auto mt-5 h-100">With us, you can shop online & help save your high street at the same time</p>
         </div>
     )
 }
