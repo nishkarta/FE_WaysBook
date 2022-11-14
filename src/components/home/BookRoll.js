f2ded1acda905f320ee83ebcff41e4c618f01043
nishkarta <nishkarta@gmail.com>
2022-11-14 14:31:40 +0700

    test

--- src/components/home/BookRoll.js
+++ src/components/home/BookRoll.js
@@ -66,7 +66,7 @@ export default function BookRoll() {
                                 </Col>
 
                                 <Col style={{}} className="px-0 mx-0 d-flex align-items-center">
-                                    <Card.Body className="bg-white me-3 py-1 px-3" style={{ borderRadius: 0, height: '14rem', width: '8rem' }}>
+                                    <Card.Body className="bg-white me-3 py-2 px-3" style={{ borderRadius: 0, height: '15rem', width: '8rem' }}>
                                         <Card.Title onClick={state.isLogin ? (() => navigate(`/detail/${item?.id}`)) : (() => setShowLog(true))} className="ff-tns fs-24 fw-bold" style={{ width: "150px", overFlow: "hidden" }}>{item?.title}</Card.Title>
                                         <Card.Text style={{ lineHeight: '2', marginBottom: '20px' }}>
                                             <span className="ff-avn fs-14" style={{ color: '#929292', fontStyle: 'italic' }}>By {item?.author}</span>
