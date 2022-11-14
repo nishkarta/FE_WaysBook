06641646e3280e5f54d20377060e5d026b362e0a
nishkarta <nishkarta@gmail.com>
2022-11-14 14:29:59 +0700

    test

--- src/components/home/BookRoll.js
+++ src/components/home/BookRoll.js
@@ -75,8 +75,8 @@ export default function BookRoll() {
                                             <br />
                                             <span className="ff-avn fs-18" style={{ color: '#44B200' }}>{convertRupiah.convert(item?.price)}</span>
                                             <br />
+                                            <Button style={{ borderRadius: 0 }} variant="dark w-100" onClick={state?.isLogin ? (() => addToCartHandler(item.id)) : (() => setShowLog(true))}>Add Cart</Button>
                                         </Card.Text>
-                                        <Button style={{ borderRadius: 0 }} variant="dark w-100" onClick={state?.isLogin ? (() => addToCartHandler(item.id)) : (() => setShowLog(true))}>Add Cart</Button>
                                     </Card.Body>
                                 </Col>
 
