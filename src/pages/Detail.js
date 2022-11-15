efced7f9652c134b8ccb2f5a615a74bab0050982
nishkarta <nishkarta@gmail.com>
2022-11-15 20:37:46 +0700

    test

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -12,15 +12,25 @@ import { UserContext } from '../components/context/userContext'
 
 export default function Detail({ item }) {
     const [showAddOrderPopup, setShowAddOrderPopup] = React.useState(false)
+    const [isLoading, setIsLoading] = React.useState(false)
     const [state, dispatch] = React.useContext(UserContext)
 
     const params = useParams()
     const navigate = useNavigate()
 
     let { data: book } = useQuery("detailCache", async () => {
+        setIsLoading(true)
         const response = await API.get(`book/${params.id}`)
+        setIsLoading(false)
         return response.data.data
     })
+
+
+    // setTimeout(() => {
+    //     setIsLoading(false)
+
+    // }, 500);
+
 
     const addToCartHandler = async (bookId) => {
         try {
@@ -34,12 +44,20 @@ export default function Detail({ item }) {
 
     return (
         <>
-            <>
-                <DetailHead book={book} />
-                <DetailAbout book={book} />
-                {state?.user.role === "cust" && <Button variant='dark' className="float-end mx-5" onClick={() => addToCartHandler(params.id)}>Add to Cart<i className="fa-solid fa-cart-shopping"></i></Button>}
 
-            </>
+            {isLoading ?
+                <div className='d-flex align-items-center justify-content-center h-60'>
+                    <p className="fs-36 ff-avn">Loading page...</p>
+                </div> : (
+                    <>
+
+                        <DetailHead book={book} />
+                        <DetailAbout book={book} />
+                        {state?.user.role === "cust" && <Button variant='dark' className="float-end mx-5" onClick={() => addToCartHandler(params.id)}>Add to Cart<i className="fa-solid fa-cart-shopping"></i></Button>}
+
+                    </>
+                )}
+
 
             <AddOrderPopup showAddOrderPopup={showAddOrderPopup} setShowAddOrderPopup={setShowAddOrderPopup} />
         </>
